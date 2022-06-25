f5a03121a9774a9476c0ea6d377be0153bf37e51
Milk Steak <a.khatttab97@gmail.com>
2022-06-25 18:55:03 +0200

    Add function tryAgain and Transition to the whole div(pic&buttons)

--- js.js
+++ js.js
@@ -1,11 +1,100 @@
 let choices = ['rock', 'paper', 'scissors'];
 let playerCounter = 0;
 let computerCounter = 0;
+let computerSelection = null;
 
 
 const buttons = Array.from(document.querySelectorAll('button'));
 buttons.forEach(button => button.addEventListener('click', playRound));
+buttons.forEach(button =>{
+   button.parentElement.addEventListener('transitionend', removeTransition)
+});
 
+const computerButtons = Array.from(document.querySelectorAll(`button[disabled]`));
+console.log(computerButtons);
+
+
+const wrapper_1 = document.querySelector('.wrapper-1');
+let playerScore = document.createElement('p');
+wrapper_1.appendChild(playerScore);
+
+const wrapper_2 = document.querySelector('.wrapper-2');
+let computerScore = document.createElement('p');
+wrapper_2.appendChild(computerScore);
+
+
+let msg = document.querySelector('#msg');
+let msgDiv = document.querySelector('.msg')
+
+
+
+function playRound(e){
+    console.log(this);
+    let button = e.target;
+    let playerSelection = button.textContent;
+    playerSelection = player(playerSelection.toLowerCase());
+    computerSelection = computerPlay();
+    console.log(button.parentElement);
+
+    button.parentElement.classList.add('pressed');
+    addClassToComputer();
+
+    switch (true){
+        case playerSelection === computerSelection:
+            msg.textContent = 'It\'s a tie';
+            console.log(msg);
+            return msg;
+        case (playerSelection === 0 || computerSelection === 0) && (playerSelection === 2 || computerSelection === 2):
+            if (playerSelection === 0){
+                playerWinMsg();
+                break;
+            }else {
+                computerWinMsg();
+                break;
+            }
+        case playerSelection > computerSelection:
+            playerWinMsg();
+            break;
+        default:
+            computerWinMsg();
+    }
+    playerScore.textContent = playerCounter;
+    computerScore.textContent = computerCounter;
+    let res = result(playerCounter, computerCounter);
+
+    if (res) tryAgain();
+
+}
+
+
+function tryAgain(){
+
+    let but = document.createElement('button');
+    but.textContent = 'Try Again';
+    but.style.cssText = "width: 10rem; height: fit-content; border-radius: 5px;"
+    but.addEventListener('click', () => {
+        buttons.forEach(button => button.removeAttribute('disabled'));
+        computerButtons.forEach(button => {
+            button.setAttribute('disabled', 'disabled');
+        });
+        playerCounter = 0;
+        computerCounter = 0;
+        playerScore.textContent = playerCounter;
+        computerScore.textContent = computerCounter;
+        msgDiv.removeChild(but);
+        msg.textContent = 'Start';
+
+    });
+    msgDiv.appendChild(but);
+
+}
+
+
+function removeTransition(e){
+    console.log(e.target);
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('pressed');
+}
 
 function computerPlay(){
     let choice = Math.floor(Math.random()*choices.length)
@@ -26,62 +115,45 @@ function player(playerSelection){
 
 function computerWinMsg(){
     computerCounter += 1;
-    let msg = 'Computer Won This Round!';
+    msg.textContent = 'Computer won this round';
     console.log(msg);
-    alert(msg);
     return msg;
 }
 
 function playerWinMsg(){
     playerCounter += 1;
-    let msg = 'You Won This Round!'
+    msg.textContent = 'You won this round!'
     console.log(msg);
-    alert(msg);
     return msg;
 }
 
-function playRound(e){
-    console.log(this);
-    let button = e.target;
-    let playerSelection = button.textContent;
-    playerSelection = player(playerSelection.toLowerCase());
-    let computerSelection = computerPlay();
-    switch (true){
-        case playerSelection === computerSelection:
-            let msg = 'It\'s A Tie';
-            console.log(msg);
-            alert(msg);
-            return msg;
-        case (playerSelection === 0 || computerSelection === 0) && (playerSelection === 2 || computerSelection === 2):
-            if (playerSelection === 0){
-                playerWinMsg();
-                break;
-            }else {
-                computerWinMsg();
-                break;
-            }
-        case playerSelection > computerSelection:
-            playerWinMsg();
-            break;
-        default:
-            computerWinMsg();
-    }
-    result(playerCounter, computerCounter);
+
+function addClassToComputer(){
+    computerButtons.forEach(button => {
+       if (choices[computerSelection] === button.textContent.toLowerCase()) {
+           button.parentElement.classList.add('pressed');
+       }
+    });
 }
+
 function result(playerCounter, computerCounter){
 
     if (playerCounter === 5 || computerCounter === 5){
         if (playerCounter === 5){
-            let msg = 'You Won The Game, Congratulations';
+            msg.textContent = 'You won the game, congratulations';
             console.log(msg);
-            alert(msg);
-            return msg;
         }else{
-            let msg = 'Sorry You Lost, Computer Won';
+            msg.textContent = 'Sorry you lost, computer won';
             console.log(msg);
-            alert(msg);
-            return msg;
         }
+        buttons.forEach(button => {
+            button.setAttribute('disabled', 'disabled');
+        });
+        return true;
     }
 }
+
+playerScore.textContent = playerCounter;
+computerScore.textContent = computerCounter;
+
 
